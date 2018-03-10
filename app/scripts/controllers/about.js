9b1646e4fc31349b379b537dcb7287f79d89bcce
Jmbala <jmbala_55@hotmail.com>
2018-03-10 14:07:52 -0600

    Angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gradeexchangerFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gradeexchangerFrontEndApp
+ */
+angular.module('gradeexchangerFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gradeexchangerFrontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gradeexchangerFrontEndApp
+ */
+angular.module('gradeexchangerFrontEndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
